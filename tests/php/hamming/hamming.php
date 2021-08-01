fa4bb608f74109a82547010b66f07e3e44cd4868
jmar <jmar_dny@hotmail.com>
2021-08-01 16:52:51 +0000

    hamming changes

--- tests/php/hamming/hamming.php
+++ tests/php/hamming/hamming.php
@@ -6,10 +6,28 @@ convenience to get you started writing code faster.
 
 Remove this comment before submitting your exercise.
 */
+$f = 'A';
+$g = 'A';
 
 function distance(string $strandA, string $strandB) : int
 {
-    //
-    // YOUR CODE GOES HERE
-    //
+    $hamDistance = 0;
+
+    if (strlen($strandA) != (strlen($strandB))){
+        //echo "DNA strands must be of equal length.";
+        throw new InvalidArgumentException("DNA strands must be of equal length.");
+    }
+    else{
+        for ($i = 0; $i < strlen($strandA); $i++){
+            if($strandA[$i] != $strandB[$i]){
+                $hamDistance += 1;
+            }
+        }
+    }
+    //print_r($hamDistance);
+    return intval($hamDistance);
 }
+
+//distance($f, $g);
+//echo "test";
+//distance();intval()
